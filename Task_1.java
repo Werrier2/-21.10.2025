a2b8ae34a444375f3571034b71baffda6124b507
Werrier2 <149078051+Werrier2@users.noreply.github.com>
2025-10-21 17:19:29 +0300

    Create Task_1.java

---
+++ Task_1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    private static int symbol(char a) {
+//        Scanner in = new Scanner(System.in);
+//        String inp = in.nextLine();
+//        char[]let = inp.toCharArray();
+        int num = (int) a;
+//        System.out.println(num);
+        boolean flag = true;
+        if(num <= 57 && num >= 48) {
+            System.out.println("Number");
+            flag = false;
+            return 0;
+        }
+        else if((num <= 90 && num >= 65) || (num >= 97 && num <= 122)){
+            System.out.println("Letter");
+            flag = false;
+            return 1;
+        }
+        if(flag){
+            System.out.println("Other char");
+            return 2;
+        }
+        return -111;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String n = in.nextLine();
+        char[]letters = n.toCharArray();
+        symbol(letters[0]);
+    }
+}
